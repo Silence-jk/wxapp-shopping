443f79610be0d3026ba855e1a96c061822f74cb6
silence-jk <silence-jk@foxmail.com>
2018-06-01 19:53:20 +0800

    feat: add detail page

---
+++ client/pages/detail/detail.js
@@ -0,0 +1,99 @@
+// pages/detail/detail.js
+const qcloud = require('../../vendor/wafer2-client-sdk/index.js')
+const config = require('../../config.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    product: {},
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getProduct(options.id)
+  },
+
+  getProduct: function(id) {
+    wx.showLoading({
+      title: '数据加载中......',
+    })
+    qcloud.request({
+      url: config.service.productDetail + id,
+      success: result => {
+        // console.log(result.data)
+        wx.hideLoading()
+
+        let data = result.data
+        //data.code 为 0 是正常
+        if (!data.code) {
+          this.setData({
+            product: data.data
+          })
+        } else {
+          setTimeout(() => {
+            wx.navigateBack()
+          }, 2000)
+        }
+      },
+      fail: result => {
+        wx.hideLoading()
+        setTimeout(() => {
+          wx.navigateBack()
+        }, 2000)
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
